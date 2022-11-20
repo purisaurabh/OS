f71cdebc977382b5b1918b343659541ba7d46ff0
Immanuel <purisaurabh2885@gmail.com>
2022-11-20 14:50:10 +0530

    Commit

--- Assignment_4/1_producer_consumer_problem.c
+++ Assignment_4/1_producer_consumer_problem.c
@@ -1,99 +1,79 @@
-#include <pthread.h>
-#include <stdlib.h>
-#include <semaphore.h>
-#include <unistd.h>
-#include <stdio.h>
-
+#include<stdio.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<semaphore.h>
+#include<unistd.h>
 #define buffer_size 10
 
-sem_t full , empty;
+sem_t full,empty;
 int buffer[buffer_size];
 pthread_mutex_t mutex;
-
 void *producer(void *p);
-void consumer(void *p);
+void *consumer(void *p);
 void insert_item(int);
-void remove_item();
-
+int remove_item();
 int counter;
+
 void initialize()
 {
-    pthread_mutex_init(&mutex , NULL);
-    sem_init(&full , 1 , 0);
-    sem_init(&empty , 1 , buffer_size);
-    counter = 0;
+  pthread_mutex_init(&mutex,NULL);
+  sem_init(&full,1,0);
+  sem_init(&empty,1,buffer_size);
+  counter=0;
 }
-
 
 int main()
 {
-    int n1 , n2;
-    printf("Enter number of producers you want to create : ");
-    scanf("%d" , &n1);
+  int n1,n2,i;
+  printf("\nEnter no. of producers you want to create:");
+  scanf("%d",&n1);
+  printf("\nEnter no. of consumers you want to create:");
+  scanf("%d",&n2);
+  initialize();
+  pthread_t tid[n1],tid1[n2];
+  for(i=0;i<n1;i++)
+  pthread_create(&tid[i],NULL,producer,NULL);
+  for(i=0;i<n2;i++)
+  pthread_create(&tid1[i],NULL,consumer,NULL);
+  sleep(50);
+  exit(0);
+}
 
-    printf("Enter number of consumers you want to create : ");
-    scanf("%d" , &n2);
+void *producer(void *p)
+{
+  int item,waittime;
+  waittime=rand()%5;
+  sleep(waittime);
+  
+  item =rand()%10;
+  sem_wait(&empty);
+  pthread_mutex_lock(&mutex);
+  printf("\n Producer produced %d item",item);
+  insert_item(item);
+  pthread_mutex_unlock(&mutex);
+  sem_post(&full);
+}
 
-    initialize();
+void *consumer(void *p)
+{
+  int item,waittime;
+  waittime=rand()%10;
+  sleep(waittime);
+  sem_wait(&full);  
+  pthread_mutex_lock(&mutex);
+  item=remove_item();
+  printf("\n Consumer consumed %d item",item);
+  
+  pthread_mutex_unlock(&mutex);
+  sem_post(&empty);
+}
 
-    pthread_t tid[n1];
-    pthread_t tid_1[n2];
+void insert_item(int item)
+{
+  buffer[counter++]=item;
+}
 
-    sleep(50);
-
-    for(int i = 0 ; i < n1 ; i++)
-    {
-        pthread_create(&tid[i] , NULL , producer , NULL);
-    }
-
-    for(int i = 0 ; i < n2 ; i++)
-    {
-        pthread_create(&tid_1[i] , NULL , consumer , NULL);
-    }
-
-
-
-    void *producer(void *p)
-    {
-        int wait_time;
-        wait_time = rand() % 10;
-        sleep(wait_time);
-
-        int item = rand() % 10;
-        sem_wait(&empty);
-
-        pthread_mutex_lock(&mutex);
-        printf("\n Producer produced %d item " , item);
-        insert_item(item);
-        pthread_mutex_unlock(&mutex);
-        sem_post(&full);
-    }
-
-
-    void consumer(void *p)
-    {
-        int wait_time;
-        wait_time = rand() % 10;
-        sleep(wait_time);
-
-        sem_wait(&full);
-        pthread_mutex_lock(&mutex);
-
-        int item = remove_item();
-        printf("\nConsumer consume %d item " , item);
-        pthread_mutex_unlock(&mutex);
-        sem_post(&empty);
-    }
-
-    void insert_item(int item)
-    {
-        buffer[counter++] = item;
-    }
-
-    int remove_item()
-    {
-        return(buffer[--counter]);
-    }
-
-
+int remove_item()
+{
+  return(buffer[--counter]);
 }
--- Assignment_7/1_b_server.c
+++ Assignment_7/1_b_server.c
@@ -10,7 +10,7 @@ int main()
 
     // create shared memory segment with key 2345 having size 1024 bytes. IPC_CREATE is used to create shared segment if it does not exits 0666 are the permissions on the shared segment
     int shmid;
-    shmget((key_t)2345 , 1024 , 0666 | IPC_CREAT);
+    shmid = shmget((key_t)2345 , 1024 , 0666 | IPC_CREAT);
     printf("Key of the shared memory is : %d" , shmid);
 
     void *shared_memory;
